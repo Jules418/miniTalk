ceeee6bc7a2304b953b8e62bcca367555624eb3f
Jules <julio34310@gmail.com>
2024-02-17 17:33:38 +0100

    almost done ig

--- srcs/client.c
+++ srcs/client.c
@@ -1,2 +1,68 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 15:48:04 by jules             #+#    #+#             */
+/*   Updated: 2024/02/17 17:33:05 by jules            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+
+#define WAIT_TIME 50
+
+int	ft_atoi(char *s)
+{
+	int	nb;
+	int	i;
+
+	nb = 0;
+	i = 0;
+	if (!s)
+		return (-1);
+	while (('0' <= s[i]) && (s[i] <= '9'))
+	{
+		nb = nb * 10 + (s[i] - '0');
+		i++;
+	}
+	return (nb);
+}
+
+void	ft_send(int pid, unsigned char c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (0x80 >> i))
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i++;
+		usleep(WAIT_TIME);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	if (argc != 3)
+		return (1);
+	pid = ft_atoi(argv[1]);
+	if (pid < 0)
+		return (1);
+	i = 0;
+	while (argv[2][i])
+	{
+		ft_send(pid, argv[2][i]);
+		i++;
+	}
+	ft_send(pid, '\n');
+}
--- srcs/server.c
+++ srcs/server.c
@@ -1,8 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 15:48:08 by jules             #+#    #+#             */
+/*   Updated: 2024/02/17 17:33:16 by jules            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+#include <signal.h>
 #include <stdio.h> // replace with ft_printf
 
-int	main()
+#define WAIT_TIME 50
+
+void	listen(int signal)
+{
+	static int	i = 0;
+	static char	c = 0;
+
+	c <<= 1;
+	i += 1;
+	if (signal == SIGUSR2)
+		c |= 1;
+	if (i == 8)
+	{
+		printf("%c", c);
+		c = 0;
+		i = 0;
+	}
+}
+
+int	main(void)
 {
 	printf("Server ID : %d\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR1, listen);
+		signal(SIGUSR2, listen);
+		pause ();
+	}
 	return (0);
 }
